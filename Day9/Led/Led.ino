a2ed8f188b612827f9c834b5b1dc7274bbe3115f
aakankshamagdum <aakankshamagdum28@gmail.com>
2025-12-23 10:37:01 +0530

    Add files via upload

---
+++ Day9/Led/Led.ino
@@ -0,0 +1,14 @@
+#define LED 2
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(LED, HIGH);
+  delay(2000);
+  digitalWrite(LED, LOW);
+  delay(2000);
+}
